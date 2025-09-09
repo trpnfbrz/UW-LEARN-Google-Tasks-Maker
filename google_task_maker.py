d322819d69c5c8a2de72ed77f667bfe53234a5ee
Joey Luo <joeyluo193@gmail.com>
2025-09-09 10:12:09 -0400

    trying to implement more api stuff

--- google_task_maker.py
+++ google_task_maker.py
@@ -6,8 +6,32 @@ import html2text
 h = html2text.HTML2Text() #create instance of HTML2Text
 h.ignore_links = True
 
+import os
 from google.auth.transport.requests import Request
- 
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+CLIENT_FILE = 'client_secret_577299332042-jc7q7nibi10kcbr2o4rhllhio6uc273m.apps.googleusercontent.com'
+API_NAME = 'tasks'
+API_VERSION = 'v1'
+SCOPES = ['https://www.googleapis.com/auth/tasks']
+
+creds = None
+
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else: 
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_FILE, SCOPES)
+        creds = flow.run_local_server(port=0)
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
 def inbox_scraper(): #function to scrape emails
     with open("credentials.yml") as f:
         content = f.read()
